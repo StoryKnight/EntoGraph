f230dc415447e3fad2b592b6f638723171a7b6e5
storyknight <realstoryknight@gmail.com>
2022-03-24 19:50:38 -0500

    initial commit

---
+++ src/main/java/com/example/ento/EntoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ento/EntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
