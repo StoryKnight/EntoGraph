11d6e4b69be64f1803d4db8ed0d9683982885f59
storyknight <realstoryknight@gmail.com>
2022-04-15 17:07:11 -0500

    add static initial files, csv file

--- src/main/java/com/example/ento/EntoApplication.java
+++ src/main/java/com/example/ento/EntoApplication.java
@@ -3,11 +3,16 @@ package com.example.ento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class EntoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntoApplication.class, args);
 	}
-
 }
